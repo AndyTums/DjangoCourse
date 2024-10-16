a356e867d8ca64c8cb3ddfa8e36b5e4d9c1440a7
Andrey Tumerkin <tumerrkin@gmail.com>
2024-10-16 22:16:25 +0300

    some fixe

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home, category, order, contact, user_contact, product_detail
@@ -13,3 +15,6 @@ urlpatterns = [
     path('product/<int:product_id>/', product_detail, name='product_detail'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
